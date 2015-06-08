26169c8d172b019c626c3f4a89cfe6bc5498edd2
chaluemwut <chaluemwut@hotmail.com>
2015-06-08 13:11:29 +0700

    test swift animation

--- SwiftAnimationLab/ViewController.swift
+++ SwiftAnimationLab/ViewController.swift
@@ -13,12 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let txt = UILabel()
-        txt.frame = CGRectMake(0, 50, 100, 20)
+        txt.frame = CGRectMake(100, 100, 100, 20)
         txt.text = "Hello World"
         self.view.addSubview(txt)
-        UIView.animateWithDuration(2.0, animations: {
-            txt.center.x += 100
-        })
+//        UIView.animateWithDuration(2.0, animations: {
+//            txt.center.x += 100
+//        })
+        
+        UIView.animateWithDuration(2.0, delay: 0.3, options: nil, animations: {
+            txt.frame.origin.y += 50
+        }, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
