b737dd16d19028afcde3b5ba1871d2ec9aff1258
chaluemwut <chaluemwut@hotmail.com>
2015-06-08 12:45:43 +0700

    Initial Commit

---
+++ SwiftAnimationLab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftAnimationLab
+//
+//  Created by Computer Engineering on 6/8/2558 BE.
+//  Copyright (c) 2558 Chaluemwut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftAnimationLabTests/SwiftAnimationLabTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftAnimationLabTests.swift
+//  SwiftAnimationLabTests
+//
+//  Created by Computer Engineering on 6/8/2558 BE.
+//  Copyright (c) 2558 Chaluemwut. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwiftAnimationLabTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
